20c1dd3418d46c617f570b801750aeabcbc8fbaf
min56 <minchaeg85@gmail.com>
2023-10-28 18:55:15 +0900

    ⚙️ :: 프로젝트 세팅

---
+++ src/main/kotlin/com/example/pensionkeybackend/PensionKeyBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pensionkeybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PensionKeyBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PensionKeyBackendApplication>(*args)
+}

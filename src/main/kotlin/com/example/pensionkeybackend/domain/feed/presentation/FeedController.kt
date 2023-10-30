7b07f297ed0d396d4008f1b3d8a2e2da38114e49
alsco39 <minchaeg85@gmail.com>
2023-10-31 00:26:53 +0900

    ⚡️ :: 게시글 작성 API

---
+++ src/main/kotlin/com/example/pensionkeybackend/domain/feed/presentation/FeedController.kt
@@ -0,0 +1,35 @@
+package com.example.pensionkeybackend.domain.feed.presentation
+
+import com.example.pensionkeybackend.domain.feed.presentation.dto.request.CreateFeedRequest
+import com.example.pensionkeybackend.domain.feed.presentation.dto.response.QueryFeedDetailsResponse
+import com.example.pensionkeybackend.domain.feed.service.CreateFeedService
+import com.example.pensionkeybackend.domain.feed.service.QueryFeedDetailsService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.validation.Valid
+
+@RequestMapping("/feed")
+@RestController
+class FeedController(
+    private val createFeedService: CreateFeedService,
+    private val queryFeedDetailsService: QueryFeedDetailsService
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    fun createFeed(@RequestBody @Valid request: CreateFeedRequest) {
+        createFeedService.execute(request)
+    }
+
+    @GetMapping("/{feed_id}")
+    fun getFeedDetails(@PathVariable(name = "feed_id") feedId: UUID) : QueryFeedDetailsResponse {
+        return queryFeedDetailsService.execute(feedId)
+    }
+}
---
+++ src/main/kotlin/com/example/pensionkeybackend/domain/feed/service/CreateFeedService.kt
@@ -0,0 +1,29 @@
+package com.example.pensionkeybackend.domain.feed.service
+
+import com.example.pensionkeybackend.domain.feed.domain.Feed
+import com.example.pensionkeybackend.domain.feed.domain.repository.FeedRepository
+import com.example.pensionkeybackend.domain.feed.presentation.dto.request.CreateFeedRequest
+import com.example.pensionkeybackend.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+
+@Service
+class CreateFeedService(
+    private val feedRepository: FeedRepository,
+    private val userFacade: UserFacade
+) {
+
+    @Transactional
+    fun execute(request: CreateFeedRequest) {
+
+        feedRepository.save(
+            Feed(
+                title = request.title,
+                content = request.content,
+                createdAt = LocalDate.now(),
+                user = userFacade.getCurrentUser()
+            )
+        )
+    }
+}

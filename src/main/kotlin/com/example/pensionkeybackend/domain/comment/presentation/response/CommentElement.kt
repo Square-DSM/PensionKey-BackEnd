adac8f709379a4026650726bd84e5b53319fdc8b
jyk1029 <jyk102999@gmail.com>
2023-11-01 09:12:12 +0900

    🐛 :: 댓글 속성 수정

--- src/main/kotlin/com/example/pensionkeybackend/domain/comment/presentation/response/CommentElement.kt
+++ src/main/kotlin/com/example/pensionkeybackend/domain/comment/presentation/response/CommentElement.kt
@@ -1,9 +1,10 @@
 package com.example.pensionkeybackend.domain.comment.presentation.response
 
+import com.example.pensionkeybackend.domain.user.domain.User
 import java.util.UUID
 
 data class CommentElement(
     val id: UUID,
     val content: String,
-    val accountId: String
+    val user: User
 )

87c265389b52395f2786ed1c6108abce870ef8dc
jyk1029 <jyk102999@gmail.com>
2023-11-01 18:52:20 +0900

    ♻️ :: 리팩토링

--- src/main/kotlin/com/example/pensionkeybackend/global/error/GlobalException.kt
+++ src/main/kotlin/com/example/pensionkeybackend/global/error/GlobalException.kt
@@ -3,6 +3,5 @@ package com.example.pensionkeybackend.global.error
 abstract class GlobalException(
     val errorProperty: ErrorProperty
 ) : RuntimeException() {
-
     override fun fillInStackTrace() = this
 }
